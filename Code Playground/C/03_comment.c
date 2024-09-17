dfa67d61c76a984530d1663391d3825055942cdc
DDHHOONNII <168064529+DDHHOONNII@users.noreply.github.com>
2024-09-17 23:49:38 +0530

    Delete Code Playground directory

--- Code Playground/C/03_comment.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int a;
-    printf("enter any number : \n");/* printf is function  C programming */
-    scanf("%d",&a); // scanf is also a function in C programming 
-    printf("your program ended sucessfully");
-    return 0;
-}
